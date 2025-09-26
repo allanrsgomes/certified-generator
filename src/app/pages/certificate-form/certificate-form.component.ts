c6f3ddab6f4a2e5500c52c6df2c458d88b735e80
Allan Roberto Da Silva Gomes <allanrs@ps.grupopanvel.com.br>
2025-09-26 17:31:05 -0300

    feat: ngModel

--- src/app/pages/certificate-form/certificate-form.component.ts
+++ src/app/pages/certificate-form/certificate-form.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { PrimaryButtonComponent } from 'src/app/_components/primary-button/primary-button.component';
@@ -6,12 +7,12 @@ import { SecundaryButtonComponent } from 'src/app/_components/secundary-button/s
 @Component({
   selector: 'app-certificate-form',
   standalone: true,
-  imports: [PrimaryButtonComponent, SecundaryButtonComponent, FormsModule],
+  imports: [PrimaryButtonComponent, SecundaryButtonComponent, FormsModule, CommonModule],
   templateUrl: './certificate-form.component.html',
   styleUrls: ['./certificate-form.component.css']
 })
 export class CertificateFormComponent {
   name: string = ''
   activity: string = ''
-  activities: string[] = []
+  activities: string[] = ['Angular', 'React']
 }
